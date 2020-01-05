ec04ea164786b48c9150ab4a581772ef6792c83d
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-04 20:21:26 -0800

    Added isEmpty() and displayList() functions

--- src/ds/singlylinkedlist/SinglyLinkedList.java
+++ src/ds/singlylinkedlist/SinglyLinkedList.java
@@ -1,4 +1,28 @@
 package ds.singlylinkedlist;
 
 public class SinglyLinkedList {
+
+    private Node first; // Represents the first node in the list
+
+
+
+    /**
+     * Function to check if the linked list is empty
+     *
+     * @return true if linked list is empty; false if not
+     */
+    public boolean isEmpty() {
+        return (first == null);
+    }
+
+    /**
+     * Displays all nodes in the list
+     */
+    public void displayList() {
+        Node current = first;
+        while (current.next != null) {
+            current.displayNode();
+            current = current.next;
+        }
+    }
 }
