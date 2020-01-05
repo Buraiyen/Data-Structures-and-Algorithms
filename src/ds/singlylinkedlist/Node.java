1ac14cbe04d02d58cc1ca248ce719d676b98bfc1
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-04 20:12:18 -0800

    Added node variables and function to display node data

--- src/ds/singlylinkedlist/Node.java
+++ src/ds/singlylinkedlist/Node.java
@@ -1,4 +1,14 @@
 package ds.singlylinkedlist;
 
 public class Node {
+    public int data;    // Numeric value inside of a node
+    public Node next;   // Represents the next node in list
+
+
+    /**
+     * Function to display value in node
+     */
+    public void displayNode() {
+        System.out.println("[ " + data + " ]");
+    }
 }
