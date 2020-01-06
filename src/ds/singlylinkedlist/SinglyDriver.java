d872687538dc42ba012eafc713cd7cae45d8c7d5
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-06 14:50:56 -0800

    Added test methods

--- src/ds/singlylinkedlist/SinglyDriver.java
+++ src/ds/singlylinkedlist/SinglyDriver.java
@@ -1,4 +1,16 @@
 package ds.singlylinkedlist;
 
 public class SinglyDriver {
+    public static void main(String[] args) {
+        SinglyLinkedList sl = new SinglyLinkedList();
+        sl.insertFirst(1);
+        sl.insertFirst(2);
+        sl.insertFirst(3);
+        sl.insertFirst(4);
+        sl.insertFirst(5);
+        sl.insertLast(100);
+        sl.displayList();
+        sl.deleteFirst();
+        sl.displayList();
+    }
 }
