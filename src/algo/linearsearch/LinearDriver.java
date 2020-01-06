9982bc688e5b7e17c73a08427ea926c02d7c0a5b
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-06 15:17:06 -0800

    Added algorithm and test array

--- src/algo/linearsearch/LinearDriver.java
+++ src/algo/linearsearch/LinearDriver.java
@@ -2,10 +2,16 @@ package algo.linearsearch;
 
 public class LinearDriver {
     public static void main(String[] args) {
-
+        int arr[] = {111111111, 3, 2, 100, 39, 232, 11, 3344, 32, 11, 94, 298, 193, 11, 332, 1111111, 22, 333333, 4};
+        System.out.println(search(arr, 4));
     }
 
-    public int search(int arr[], int x) {
-        
+    public static int search(int arr[], int x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
