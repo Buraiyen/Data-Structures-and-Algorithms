7d0ca7a8600e1138b4ebcd24b4b3541fd77f26e9
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-06 15:18:10 -0800

    Added comments

--- src/algo/linearsearch/LinearDriver.java
+++ src/algo/linearsearch/LinearDriver.java
@@ -6,6 +6,12 @@ public class LinearDriver {
         System.out.println(search(arr, 4));
     }
 
+    /**
+     * 
+     * @param arr - the array used to search for integer
+     * @param x - the value that the array is searching for
+     * @return - the index where the value is found. -1 if not found
+     */
     public static int search(int arr[], int x) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == x) {
