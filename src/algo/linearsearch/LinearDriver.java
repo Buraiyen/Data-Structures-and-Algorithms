63634e8e41fc687111ff01c5a287f39e75b7adc9
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-06 15:23:28 -0800

    Init user input

--- src/algo/linearsearch/LinearDriver.java
+++ src/algo/linearsearch/LinearDriver.java
@@ -1,13 +1,27 @@
 package algo.linearsearch;
 
+import java.util.Scanner;
+
 public class LinearDriver {
     public static void main(String[] args) {
-        int arr[] = {111111111, 3, 2, 100, 39, 232, 11, 3344, 32, 11, 94, 298, 193, 11, 332, 1111111, 22, 333333, 4};
-        System.out.println(search(arr, 4));
+        int size, min, max, value;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the size of your array: ");
+        size = in.nextInt();
+
+        System.out.print("Enter the minimum value of the range greater than zero: ");
+        min = in.nextInt();
+
+        System.out.print("Enter the maximum value of the range: ");
+        min = in.nextInt();
+
+        System.out.print("Enter the value you would like to search for: ");
+        value = in.nextInt();
     }
 
     /**
-     * 
+     *
      * @param arr - the array used to search for integer
      * @param x - the value that the array is searching for
      * @return - the index where the value is found. -1 if not found
