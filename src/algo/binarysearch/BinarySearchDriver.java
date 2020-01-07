71f3bdd633c43a4ba156ffc14bbedfdebc2af921
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-06 21:02:55 -0800

    Created binary search algorithm

--- src/algo/binarysearch/BinarySearchDriver.java
+++ src/algo/binarysearch/BinarySearchDriver.java
@@ -1,4 +1,21 @@
 package algo.binarysearch;
 
 public class BinarySearchDriver {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int binarySearch(int[] arr, int x) {
+        int left = 0;
+        int right = arr.length;
+        int middle;
+        while (left <= right) {
+            middle = (left + right) / 2;
+            if (arr[middle] == x) return middle;
+            if (arr[middle] > x) right = middle - 1;
+            if (arr[middle] < x) left = middle + 1;
+        }
+        return -1;
+    }
 }
