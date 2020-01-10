a743260944e89d14302caa21db2ddb05cfb1cd62
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-09 17:58:10 -0800

    Init insertFirst() and isEmpty() methods

--- src/ds/circularlinkedlist/CircularLinkedList.java
+++ src/ds/circularlinkedlist/CircularLinkedList.java
@@ -7,4 +7,19 @@ public class CircularLinkedList {
         first = null;
         last = null;
     }
+
+    public void insertFirst(int data) {
+        Node newNode = new Node();
+        newNode.data = data;
+
+        if (isEmpty()) {
+            last = newNode;
+        }
+        newNode.next = first;
+        first = newNode;
+    }
+
+    public boolean isEmpty() {
+        return (first == null);
+    }
 }
